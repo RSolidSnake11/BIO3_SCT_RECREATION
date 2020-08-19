4e37d8d22abf49e9f7ec981e67c8245cedb9e088
RSolidSnake11 <48284571+RSolidSnake11@users.noreply.github.com>
2020-08-19 10:11:42 +0200

    Update flag.h

--- PROG/flag.h
+++ PROG/flag.h
@@ -1,13 +1,13 @@
-//==========================================================================//
-//																			//
-//		Biohazard19															//
-//		Copyright (c) 1995 CAPCOM Co.,Ltd. All Rights Reserved.				//
-//																			//
-//		flag.h 																//
-//										Programmed by (?)					//
-//																			//
-//		Reconstructed by SolidSnake11 26/01/2020
-//==========================================================================//
+//============================================================================//
+//                                                                            //
+//        Biohazard19                                                         //
+//        Copyright (c) 1995 CAPCOM Co.,Ltd. All Rights Reserved.             //
+//                                                                            //
+//        flag.h                                                              //
+//                                        Programmed by (?)                   //
+//                                                                            //
+//        Reconstructed by SolidSnake11 26/01/2020
+//============================================================================//
 
 
 
@@ -15,6 +15,6 @@
 
 
 //R11C
-#define 	F_EVENT0_AOT_NO 			0x0a
-#define 	F_EVENT1_AOT_NO 			0x0b
-#define 	IR11C_OIL_ADD 				0x23
+#define     F_EVENT0_AOT_NO             0x0a
+#define     F_EVENT1_AOT_NO             0x0b
+#define     IR11C_OIL_ADD                 0x23
